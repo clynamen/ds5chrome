95d22411f34075783c1eb255fcf1b4b1d11c6e98
clynamen <clynamen@gmail.com>
2021-02-03 20:09:26 +0100

    add support for DS5 over usb on linux

--- ds5.js
+++ ds5.js
@@ -1,9 +1,48 @@
+console.log("YEYE BITCHES")
+
+
+const apply_dpad_transformation_ds5_other_cases = (initialGamepads, dualsenseIndex) => {
+    const PRESSED_BUTTON = {pressed: true, touched: true, value: 1}
+    const UNPRESSED_BUTTON = {pressed: false, touched: false, value: 0}
+    currentAxis = 9
+    // DPad is Mapped to an Axis... this converts the axis into the DPad Values
+    if (initialGamepads[dualsenseIndex].axes[currentAxis] == 1.2857143878936768) {
+        // Reset all DPad Values
+        emulatedDualSense.buttons[12] = UNPRESSED_BUTTON;
+        emulatedDualSense.buttons[13] = UNPRESSED_BUTTON;
+        emulatedDualSense.buttons[14] = UNPRESSED_BUTTON;
+        emulatedDualSense.buttons[15] = UNPRESSED_BUTTON;
+    } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == 0.14285719394683838) {
+        if (PRESSED_BUTTON) {
+            emulatedDualSense.buttons[13] = PRESSED_BUTTON;
+            emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
+        }
+    } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == -0.4285714030265808) {
+        if (PRESSED_BUTTON) {
+            emulatedDualSense.buttons[15] = PRESSED_BUTTON;
+            emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
+        }
+    } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == 0.7142857313156128) {
+        if (PRESSED_BUTTON) {
+            emulatedDualSense.buttons[14] = PRESSED_BUTTON;
+            emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
+        }
+    } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == -1) {
+        if (PRESSED_BUTTON) {
+            emulatedDualSense.buttons[12] = PRESSED_BUTTON;
+            emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
+        }
+    }
+}
+
 const setupDualSenseController = () => {
+    const PRESSED_BUTTON = {pressed: true, touched: true, value: 1}
+    const UNPRESSED_BUTTON = {pressed: false, touched: false, value: 0}
     const buttonCount = 17;
     const axis = 10;
 
     const emulatedDualSense = {
-        id: "DualSense 5 Controller (STANDARD GAMEPAD Vendor: 054c Product: 09cc)",
+        id: "DualSense 5 Controller",
         index: 0,
         connected: true,
         timestamp: 0,
@@ -11,17 +50,24 @@ const setupDualSenseController = () => {
         axes: [0, 0, 0, 0],
         buttons: new Array(buttonCount).fill().map(m => ({pressed: false, touched: false, value: 0}))
     }
-    let pressedButton;
-    let unpressedButton;
+    let ds5_on_linux_via_usb = false;
+    let ds5_other_cases = false;
 
     const initialGetGamepads = navigator.getGamepads;
     navigator.getGamepads = function() {
         const initialGamepads = initialGetGamepads.apply(navigator);
         let dualsenseIndex = 5;
         for (let index = 0; index < 4; index++) {
-            if (initialGamepads[index] !== null && ((initialGamepads[index].id == "Wireless Controller (Vendor: 054c Product: 0ce6)") || (initialGamepads[index].id == "DUALSHOCK 4 Wireless Controller (Vendor: 054c Product: 0ce6)"))) {
+            if (initialGamepads[index] !== null ) {
+                ds5_on_linux_via_usb = initialGamepads[index].id == "Sony Interactive Entertainment Wireless Controller (Vendor: 054c Product: 0ce6)"
+                ds5_other_cases = ( (initialGamepads[index].id == "Wireless Controller (Vendor: 054c Product: 0ce6)") 
+                        || (initialGamepads[index].id == "DUALSHOCK 4 Wireless Controller (Vendor: 054c Product: 0ce6)") )
+            }
+
+            if ( ds5_on_linux_via_usb || ds5_other_cases ) {
                 dualsenseIndex = index;
                 emulatedDualSense.index = dualsenseIndex;
+                break;
             }
         }
 
@@ -46,80 +92,34 @@ const setupDualSenseController = () => {
                 }
             }
 
-            // Can't seem to create GamepadButton so just storing a preseed and unpressed button press to use later on
-            if (emulatedDualSense.buttons[button].pressed && !pressedButton) {
-                pressedButton = emulatedDualSense.buttons[button];
-            } else if (!emulatedDualSense.buttons[button].pressed && !unpressedButton) {
-                unpressedButton = emulatedDualSense.buttons[button];
-            }
         }
 
         for(let currentAxis = 0; currentAxis < axis; currentAxis++){
             if (initialGamepads[dualsenseIndex] && initialGamepads[dualsenseIndex].axes[currentAxis]) {
-                if (currentAxis > 2) {
-                    if (currentAxis == 9) {
-                        // DPad is Mapped to an Axis... this converts the axis into the DPad Values
-                        const [dpadU, dpadD, dpadL, dpadR] = [12, 13, 14, 15];
-                        if (initialGamepads[dualsenseIndex].axes[currentAxis] == 1.2857143878936768) { // default, none pressed
-                            // Reset all DPad Values
-                            emulatedDualSense.buttons[dpadU] = unpressedButton;
-                            emulatedDualSense.buttons[dpadD] = unpressedButton;
-                            emulatedDualSense.buttons[dpadL] = unpressedButton;
-                            emulatedDualSense.buttons[dpadR] = unpressedButton;
-                        } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == 0.14285719394683838) { // down
-                            if (pressedButton) {
-                                emulatedDualSense.buttons[dpadD] = pressedButton;
-                                emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
-                            }
-                        } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == 0.4285714626312256) { // down + left
-                            if (pressedButton) {
-                                emulatedDualSense.buttons[dpadD] = pressedButton;
-                                emulatedDualSense.buttons[dpadL] = pressedButton;
-                                emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
-                            }
-                        } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == -0.1428571343421936) { // down + right
-                            if (pressedButton) {
-                                emulatedDualSense.buttons[dpadD] = pressedButton;
-                                emulatedDualSense.buttons[dpadR] = pressedButton;
-                                emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
-                            }
-                        } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == -0.4285714030265808) { // right
-                            if (pressedButton) {
-                                emulatedDualSense.buttons[dpadR] = pressedButton;
-                                emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
-                            }
-                        } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == 0.7142857313156128) { // left
-                            if (pressedButton) {
-                                emulatedDualSense.buttons[dpadL] = pressedButton;
-                                emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
-                            }
-                        } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == -1) { // up
-                            if (pressedButton) {
-                                emulatedDualSense.buttons[dpadU] = pressedButton;
-                                emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
-                            }
-                        } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == 1) { // up + left
-                            if (pressedButton) {
-                                emulatedDualSense.buttons[dpadU] = pressedButton;
-                                emulatedDualSense.buttons[dpadL] = pressedButton;
-                                emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
-                            }
-                        } else if (initialGamepads[dualsenseIndex].axes[currentAxis] == -0.7142857313156128) { // up + right
-                            if (pressedButton) {
-                                emulatedDualSense.buttons[dpadU] = pressedButton;
-                                emulatedDualSense.buttons[dpadR] = pressedButton;
-                                emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
-                            }
-                        }
-                    } else if (currentAxis == 5) {
-                        emulatedDualSense.axes[3] = Math.min(Math.max((initialGamepads[dualsenseIndex].axes[currentAxis] * 1), -1), 1);
-                        emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
-                    }
-                } else {
+                if (currentAxis < 2) {
                     emulatedDualSense.axes[currentAxis] = Math.min(Math.max((initialGamepads[dualsenseIndex].axes[currentAxis] * 1), -1), 1);
                     emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
                 }
+                if (currentAxis == 9 && ds5_other_cases) {
+                    apply_dpad_transformation_ds5_other_cases(initialGamepads, dualsenseIndex);
+                } else if (currentAxis == 5) {
+                    emulatedDualSense.axes[3] = Math.min(Math.max((initialGamepads[dualsenseIndex].axes[currentAxis] * 1), -1), 1);
+                    emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
+                }
             }
+        }
+
+        if (ds5_on_linux_via_usb) {
+            emulatedDualSense.buttons[12] = (initialGamepads[dualsenseIndex].axes[7] < 0) ? PRESSED_BUTTON : UNPRESSED_BUTTON;
+            emulatedDualSense.buttons[13] = (initialGamepads[dualsenseIndex].axes[7] > 0) ? PRESSED_BUTTON : UNPRESSED_BUTTON;
+            emulatedDualSense.buttons[14] = (initialGamepads[dualsenseIndex].axes[6] < 0) ? PRESSED_BUTTON : UNPRESSED_BUTTON;
+            emulatedDualSense.buttons[15] = (initialGamepads[dualsenseIndex].axes[6] > 0) ? PRESSED_BUTTON : UNPRESSED_BUTTON;
+
+            emulatedDualSense.buttons[16] = (initialGamepads[dualsenseIndex].buttons[12].pressed) ? PRESSED_BUTTON : UNPRESSED_BUTTON;
+            emulatedDualSense.buttons[17] = (initialGamepads[dualsenseIndex].buttons[13].pressed) ? PRESSED_BUTTON : UNPRESSED_BUTTON;
+            emulatedDualSense.buttons[18] = (initialGamepads[dualsenseIndex].buttons[14].pressed) ? PRESSED_BUTTON : UNPRESSED_BUTTON;
+
+            emulatedDualSense.timestamp = initialGamepads[dualsenseIndex].timestamp;
         }
 
         // Only Apply Emulated Controller on DS5 Controllers
